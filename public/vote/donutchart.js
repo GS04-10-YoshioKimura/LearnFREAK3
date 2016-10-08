82b400c217beaab49aa6199332667c1f3b9807b4
GS04-10-YoshioKimura <thk.rspt.csd@gmail.com>
2016-10-08 20:54:50 +0900

    14th commit

--- public/vote/donutchart.js
+++ public/vote/donutchart.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     var radiobtn1 = document.getElementById("off");
     var radiobtn2 = document.getElementById("on");
 
@@ -13,33 +13,36 @@ $(function () {
     var dontknow = 100 - know;
     var percent = document.getElementById("percent");
     percent.innerHTML = "みんなの理解度は " + know + " %";
-    var doughnutData = [
-        {
-            value: know,
-            color: "#fbaa6e"
-            },
-        {
-            value: dontknow,
-            color: "#aaf2fb"
-            }
-        ];
+    var doughnutData = [{
+        value: know,
+        color: "#fbaa6e"
+    }, {
+        value: dontknow,
+        color: "#aaf2fb"
+    }];
     var myDoughnut = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
+    ctx.canvas.width = 300;
+    ctx.canvas.height = 150;
+    mychart = new Chart(ctx).Line(data_hoge1, option);
+
+    mychart.destroy();
+
+    ctx.canvas.width = 300;
+    ctx.canvas.height = 150;
+    mychart = new Chart(ctx).Line(data_hoge2, option);
 
 
-    $("#btnng,#btnok").on('click', function () {
+    $("#btnng,#btnok").on('click', function() {
         know = Math.floor(Math.random() * 100) + 1;
         dontknow = 100 - know;
         percent.innerHTML = "みんなの理解度は " + know + " %";
-        doughnutData = [
-            {
-                value: know,
-                color: "#fbaa6e"
-                },
-            {
-                value: dontknow,
-                color: "#aaf2fb"
-                }
-            ];
+        doughnutData = [{
+            value: know,
+            color: "#fbaa6e"
+        }, {
+            value: dontknow,
+            color: "#aaf2fb"
+        }];
         myDoughnut = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
     });
 });
