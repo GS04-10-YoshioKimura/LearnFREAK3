b9b44be97cfd10bd5413b08a4a32965bba86a033
GS04-10-YoshioKimura <thk.rspt.csd@gmail.com>
2016-10-10 04:21:39 +0900

    いいかんじのとこまで行けそう

--- public/vote/donutchart.js
+++ public/vote/donutchart.js
@@ -1,48 +1,65 @@
 $(function() {
-    var radiobtn1 = document.getElementById("off");
-    var radiobtn2 = document.getElementById("on");
 
-    // if (radiobtn1.checked == true) {
-    //     //alert("offがチェックされています。");
-    // } else if (radiobtn2.checked == true) {
-    //     //alert("onがチェックされています。");
-    // }
+    var socket = io.connect();
+    var $count = $('#count');
+    var $goods = $('#goo');
 
-    var know;
-    know = Math.floor(Math.random() * 100) + 1;
-    var dontknow = 100 - know;
-    var percent = document.getElementById("percent");
-    percent.innerHTML = "みんなの理解度は " + know + " %";
-    var doughnutData = [{
-        value: know,
-        color: "#fbaa6e"
-    }, {
-        value: dontknow,
-        color: "#aaf2fb"
-    }];
-    var myDoughnut = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
-    ctx.canvas.width = 300;
-    ctx.canvas.height = 150;
-    mychart = new Chart(ctx).Line(data_hoge1, option);
+    socket.on('count', function(setsuzoku) {
+        //alert(setsuzoku);
+        var ddd = " " + setsuzoku / 2;
+        $count.text(ddd);
 
-    mychart.destroy();
-
-    ctx.canvas.width = 300;
-    ctx.canvas.height = 150;
-    mychart = new Chart(ctx).Line(data_hoge2, option);
+        
 
 
-    $("#btnng,#btnok").on('click', function() {
-        know = Math.floor(Math.random() * 100) + 1;
-        dontknow = 100 - know;
+
+        var radiobtn1 = document.getElementById("off");
+        var radiobtn2 = document.getElementById("on");
+
+        // if (radiobtn1.checked == true) {
+        //     //alert("offがチェックされています。");
+        // } else if (radiobtn2.checked == true) {
+        //     //alert("onがチェックされています。");
+        // }
+
+        var know;
+        //know = Math.floor(Math.random() * 100) + 1;
+        know = 1 / ddd * 100;
+        var dontknow = 100 - know;
+        var percent = document.getElementById("percent");
         percent.innerHTML = "みんなの理解度は " + know + " %";
-        doughnutData = [{
+        var doughnutData = [{
             value: know,
             color: "#fbaa6e"
         }, {
             value: dontknow,
             color: "#aaf2fb"
         }];
-        myDoughnut = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
+        var ctx = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
+        // ctx.canvas.width = 300;
+        // ctx.canvas.height = 150;
+        // mychart = new Chart(ctx).Line(data_hoge1, option);
+        //
+        // mychart.destroy();
+        //
+        // ctx.canvas.width = 300;
+        // ctx.canvas.height = 150;
+        // mychart = new Chart(ctx).Line(data_hoge2, option);
+
+
+        $("#btnng,#btnok").on('click', function() {
+            //know = Math.floor(Math.random() * 100) + 1;
+            //know = data.good / ddd *100;
+            dontknow = 100 - know;
+            percent.innerHTML = "みんなの理解度は " + know + " %";
+            doughnutData = [{
+                value: know,
+                color: "#fbaa6e"
+            }, {
+                value: dontknow,
+                color: "#aaf2fb"
+            }];
+            ctx = new Chart(document.getElementById("ugCanvas").getContext("2d")).Doughnut(doughnutData);
+        });
     });
 });
